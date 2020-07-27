7593046c7c4345fd442c51dd7b244b1f4465281b
Knight <knightnibo@gmail.com>
2020-07-27 20:48:02 +0800

    fast deploy added

--- install/pyvirtualenv_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-pyenv virtualenv-delete dbMonitor
-pyenv virtualenv 3.7.2 dbMonitor
-pyenv virtualenv-init -
-pyenv init
-. ~/.pyenv/versions/dbMonitor/bin/activate
-pip install --upgrade pip
-pip install -r ../conf/requirements.txt
